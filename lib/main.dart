39b265960f7d4de575ce50dd77ef5d253a1850a8
@manuelduarte077 <carlosd99m@gmail.com>
2022-05-10 09:33:28 -0600

    chore: adding support for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:notepad/app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'package:notepad/app/app.dart';
+import 'package:notepad/firebase_options/firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
