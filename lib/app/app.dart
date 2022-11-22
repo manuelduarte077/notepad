a9a64f1f8cfddef76986f2c4863b89ee6137b4c9
Manuel Duarte  <carlosd99m@gmail.com>
2022-11-22 00:28:33 -0600

    feat: share pluss (#7)
    * create splash screen
    
    * add share pluss

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:asuka/asuka.dart' as asuka;
+import 'package:asuka/asuka.dart';
 
-import "package:notepad/features/home/home_page.dart";
+import 'package:notepad/features/splash/splash_screen.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      builder: asuka.builder,
+    return MaterialApp(
+      builder: Asuka.builder,
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: const SplashScreen(),
     );
   }
 }
