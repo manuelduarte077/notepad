df2a87fccf0fc8d524b12f016d4b2f2b5bb19803
Don Manuel <carlosd99m@gmail.com>
2024-03-26 13:54:23 -0600

    Update button styles and typography

--- lib/presentation/theme/theme.dart
+++ lib/presentation/theme/theme.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'colors.dart';
 
 class AppTheme {
   static ThemeData light = ThemeData(
     useMaterial3: true,
-    appBarTheme: AppBarTheme(
-      actionsIconTheme: const IconThemeData(color: AppColors.white),
+    appBarTheme: const AppBarTheme(
+      actionsIconTheme: IconThemeData(color: AppColors.white),
       centerTitle: false,
       elevation: 0,
-      titleTextStyle: GoogleFonts.redHatDisplay(
-        fontSize: 17,
-        fontWeight: FontWeight.bold,
-        color: AppColors.title,
-      ),
-      iconTheme: const IconThemeData(color: AppColors.title),
+      iconTheme: IconThemeData(color: AppColors.title),
     ),
-    fontFamily: GoogleFonts.redHatDisplay().fontFamily,
     iconTheme: const IconThemeData(color: AppColors.white),
     textSelectionTheme: const TextSelectionThemeData(
       cursorColor: AppColors.primary,
